2aecda2c051e7b5992fd4cfc64c0d41029bde634
mikhailveselov <mikhail.veselov.personal@gmail.com>
2026-03-31 13:41:19 +0200

    Added solution for Caesar Cipher task

---
+++ caesar_cipher.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def caesar_cipher(string, shift)
+  lowercase = ('a'..'z').to_a
+  uppercase = ('A'..'Z').to_a
+  result = ''
+
+  string.each_char do |char|
+    if lowercase.include?(char)
+      old_index = lowercase.index(char)
+      new_index = (old_index + shift) % 26
+      result << lowercase[new_index]
+    elsif uppercase.include?(char)
+      old_index = lowercase.index(char)
+      new_index = (old_index + shift) % 26
+      result << uppercase[new_index]
+    else
+      result << char
+
+    end
+  end
+  result
+end
+
+puts caesar_cipher('bla bla, bla!', -1)
